c884775754d8db4747f7277cf518e3b5302b2783
TheAdich <adidadhich2003@gmail.com>
2024-04-18 19:39:51 +0530

    saved

---
+++ backend/helpers/database.js
@@ -0,0 +1,14 @@
+
+const mariadb = require('mariadb');
+
+// Expose the Pool object within this module
+module.exports = Object.freeze({
+    pool: mariadb.createPool({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database:process.env.DB_NAME
+
+    })
+});
